00777844967218610ce67ce3bd9b27221b38fd6e
Santaguida <santaguidafernando@gmail.com>
2015-03-20 18:20:03 -0300

    Ignorar

--- CONSULTA/php/sistema/header.php
+++ CONSULTA/php/sistema/header.php
@@ -1,3 +1,4 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="estilo.css" rel="stylesheet" type="text/css" />
