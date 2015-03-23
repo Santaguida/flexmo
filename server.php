eb04cd1b5ab24b26351fce7b05dee28134d30a3b
Santaguida <santaguidafernando@gmail.com>
2015-03-23 08:34:19 -0300

    Update configs

--- server.php
+++ server.php
@@ -2,18 +2,15 @@
 
 if( $_SERVER['SERVER_NAME'] == 'localhost' ||
     $_SERVER['SERVER_NAME'] == '127.0.0.1' ||
+    $_SERVER['SERVER_NAME'] == 'saont167' ||
+    $_SERVER['SERVER_NAME'] == '10.112.0.210' ||
     $_SERVER['SERVER_NAME'] == 'flexmo')
 {
-define('DB_HOST', '127.0.0.1', true);
-define('DB_USER', 'root', true);
-define('DB_PASS', '', true);
-define('DB_BASE', 'flexmo_db', true);
-}else   { // My personal server
-define('DB_HOST', '179.188.16.12', true);
-define('DB_USER', 'fernandohs41', true);
-define('DB_PASS', 'flex123', true);
-define('DB_BASE', 'fernandohs41', true);
-        }
+    define('DB_HOST', '127.0.0.1', true);
+    define('DB_USER', 'root', true);
+    define('DB_PASS', '', true);
+    define('DB_BASE', 'flexmo_db', true);
+} 
 $db_link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die(mysql_error());
 mysqli_select_db($db_link, DB_BASE);
 
@@ -23,7 +20,6 @@ function check_data($query){
     return $result;
 }
 
-/*
- * Test
- * print_r($_SERVER);
- */
+/* Test
+print_r($_SERVER);
+*/
