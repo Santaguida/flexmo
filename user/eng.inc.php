c3fb4085f50d96bd9bb4eaeb2dce146cf791d10d
Santaguida <santaguidafernando@gmail.com>
2015-03-27 23:42:10 -0300

    Translation update continued

---
+++ user/eng.inc.php
@@ -0,0 +1,52 @@
+<?php
+
+// Language packing English | user control page
+
+$comm = array(
+    'completed'     => 'Successfully saved information !<br />',
+    'fieldError'    => 'Please fill',
+    'name'          => ' YOUR NAME*',
+    'name2'         => ' the NAME field [minimum of 3 characters]',        
+    'lastName'      => ' YOUR LAST NAME*',
+    'lastName2'     => ' the LAST NAME field [minimum of 3 characters]',
+    'user'          => ' YOUR USERNAME * [Ex: saoftron]',
+    'user2'         => " the USER field as in the example: saoNXXXX <br/>
+                        where 'are' is mandatory in inínio, 'N' is the <br/>
+                        first character of your name and 'XXXX' are the four <br/>
+                        first digits of your last name <br />",
+    'ext'           => 'YOUR EXTENSION [only with numbers]',
+    'ext2'          => ' the EXTENSION field with 4 digits.',
+    'register'      => 'YOUR REGISTRATION*',
+    'register2'     => ' the REGISTRATION field [only with numbers]',
+    'register3'     => ' the REGISTRATION field [maximum 6-digit]',
+    'badge'         => 'YOUR BADGE [only with numbers]',
+    'badge2'        => ' BADGE the [maximum 6-digit]',
+    'phone'         => 'YOUR PHONE * [example: 1540096200]',
+    'phone2'        => ' the PHONE field only with numbers and without space. <br />',
+    'phone3'        => ' The PHONE field with 10 digits <br /> Ex: DDXXXXYYYY <br />',
+    'cell'          => 'YOUR PHONE * [example: 15996096200]',
+    'cell2'         => ' field MOBILE PHONE only with numbers and without space. <br />',
+    'cell3'         => ' CELLULAR the field with 11 digits <br /> Ex: DD9XXXXYYYY <br />',
+    'email'         => 'SEU E-MAIL* [ ...@flextronics.com de preferência ]',
+    'email2'        => 'CONFIRME SEU EMAIL*',
+    'adress'        => 'SEU ENDEREÇO* [ Rua, número e compremento]',
+    'neigh'         => 'SEU BAIRRO*',
+    'city'          => 'SUA CIDADE/ESTADO*',
+    'pass'          => 'SUA SENHA*[ MÍNIMO DE 6 DIGITOS ]',
+    'pass2'         => ' o campo SENHA com no mínimo 6 caracteres.<br />',
+    'pass3'         => 'CONFIRME SUA SENHA*',
+    'func'          => ' SUA FUNÇÃO*',
+    'shift'         => ' SEU TURNO*',
+    'pgTitle'       => 'Controle de usuários',
+    'alerted'       => 'Campo obrigatório*',    
+    'manager'       => 'Supervisor',
+    'tecCoo'        => 'Coordenador técnico',
+    'prodCoo'       => 'Coordenador de produção',
+    'engineer'      => 'Engenheiro',
+    'tech'          => 'Técnico',
+    'quick'         => 'Quick Repair',
+    's1'            => 'Primeiro turno',
+    's2'            => 'Segundo turno',
+    's3'            => 'Terceiro turno',
+    's4'            => 'Administrativo'
+);
