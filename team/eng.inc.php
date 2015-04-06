76ec99c121c400a88a9c91e48b7c725599611d10
Santaguida <santaguidafernando@gmail.com>
2015-04-06 14:29:56 -0300

    Update [Edit Team]

---
+++ team/eng.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+// Language packing English | user control page
+
+$comm = array(
+    'completed'     => 'Successfully saved information !<br />',
+    'fieldError'    => 'Please fill',
+    'name'          => ' YOUR NAME*',
+    'name2'         => ' the NAME field [minimum of 3 characters]',        
+    'lastName'      => ' YOUR LAST NAME*',
+    'lastName2'     => ' the LAST NAME field [minimum of 3 characters]',
+    'user'          => ' YOUR USERNAME * [Exemple: saoftron]',
+    'user2'         => " the USER field as in the example: saoNXXXX <br/>
+                        where 'are' is mandatory early, 'N' is the <br/>
+                        first character of your name and 'XXXX' are the four <br/>
+                        first digits of your last name <br />",
+    'ext'           => 'YOUR EXTENSION [only with numbers]',
+    'ext2'          => ' the EXTENSION field with 4 digits.',
+    'register'      => 'YOUR REGISTER NAMBER*',
+    'register2'     => ' the REGISTRATION field [only with numbers]',
+    'register3'     => ' the REGISTRATION field [maximum 6-digit]',
+    'badge'         => 'YOUR BADGE [only with numbers]',
+    'badge2'        => ' the BADGE [maximum 6-digit]',
+    'phone'         => 'YOUR HOME PHONE * [example: 1540096200]',
+    'phone2'        => ' the HOME PHONE field only with numbers and without space. <br />',
+    'phone3'        => ' The HOME PHONE field with 10 digits <br /> Ex: DDXXXXYYYY <br />',
+    'cell'          => 'YOUR MOBILE PHONE * [example: 15996096200]',
+    'cell2'         => ' field MOBILE PHONE only with numbers and without space. <br />',
+    'cell3'         => ' the MOBILE PHONE field with 11 digits <br /> Ex: DD9XXXXYYYY <br />',
+    'email'         => 'YOUR E-MAIL * [... @ flextronics.com preference]',
+    'email2'        => 'CONFIRM YOUR EMAIL *',
+    'adress'        => 'YOUR ADDRESS * [Street, number and complement]',
+    'neigh'         => 'YOUR NEIGHBORHOOD*',
+    'city'          => 'YOUR CITY / STATE*',
+    'pass'          => 'YOUR PASSWORD * [6 MINIMUM DIGITS]',
+    'pass2'         => ' PASSWORD [minimum 6 characters] <br />',
+    'pass3'         => 'CONFIRM PASSWORD *',
+    'func'          => 'YOUR JOB *',
+    'shift'         => 'YOUR SHIFT *',
+    'pgTitle'       => 'User Control',
+    'alerted'       => 'Required field*',    
+    'manager'       => 'Manager',
+    'tecCoo'        => 'Technical coordinator',
+    'prodCoo'       => 'Production coordinator',
+    'engineer'      => 'Engineer',
+    'tech'          => 'Technician',
+    'quick'         => 'Quick Repair',
+    's1'            => '[1S] First shift',
+    's2'            => '[2S] Second shift',
+    's3'            => '[3S] Third shift',
+    's4'            => 'ADM',
+    'save'          =>  'SAVE',
+    'login'         => 'Username already registered'
+);
---
+++ team/ptbr.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+// Language packing portuguese Br | user control page
+
+$comm = array(
+    'completed'     => 'Informações salvas com sucesso !<br />',
+    'fieldError'    => 'Por favor, preencha',
+    'name'          => ' SEU NOME*',
+    'name2'         => ' o campo NOME [ mínimo 3 caracteres ]',        
+    'lastName'      => ' SEU SOBRENOME*',
+    'lastName2'     => ' o campo SOBRENOME [ mínimo 3 caracteres ]',
+    'user'          => ' SEU NOME DE USUÁRIO* [ Ex: saoftron ]',
+    'user2'         => " o campo USER como no exemplo: saoNXXXX,<br/>
+                        onde 'sao' é obrigatório no inínio, 'N' é o<br/>
+                        primeiro caracter do seu nome e 'XXXX' são os quatro<br/>
+                        primeiros digitos do seu sobrenome<br />",
+    'ext'           => 'SEU RAMAL [apenas com números]',
+    'ext2'          => ' o campo RAMAL com 4 dígitos.',
+    'register'      => 'SUA MATRÍCULA*',
+    'register2'     => ' o campo MATRÍCULA [apenas com números]',
+    'register3'     => ' o campo MATRÍCULA [máximo 6 dígitos]',
+    'badge'         => 'SEU BADGE [apenas com números]',
+    'badge2'        => ' o campo BADGE [máximo 6 dígitos]',
+    'phone'         => 'SEU TELEFONE* [exemplo: 1540096200 ]',
+    'phone2'        => ' o campo TELEFONE apenas com números e sem espaço.<br />',
+    'phone3'        => ' o campo TELEFONE com 10 dígitos.<br />Ex: DDXXXXYYYY<br />',
+    'cell'          => 'SEU CELULAR* [exemplo: 15996096200 ]',
+    'cell2'         => ' o campo CELULAR apenas com números e sem espaço.<br />',
+    'cell3'         => ' o campo CELULAR com 11 dígitos.<br />Ex: DD9XXXXYYYY<br />',
+    'email'         => 'SEU E-MAIL* [ ...@flextronics.com de preferência ]',
+    'email2'        => 'CONFIRME SEU EMAIL*',
+    'adress'        => 'SEU ENDEREÇO* [ Rua, número e complemento]',
+    'neigh'         => 'SEU BAIRRO*',
+    'city'          => 'SUA CIDADE/ESTADO*',
+    'pass'          => 'SUA SENHA*[ MÍNIMO DE 6 DIGITOS ]',
+    'pass2'         => ' SENHA [ mínimo 6 caracteres ]<br />',
+    'pass3'         => 'CONFIRME SUA SENHA*',
+    'func'          => ' SUA FUNÇÃO*',
+    'shift'         => ' SEU TURNO*',
+    'pgTitle'       => 'Controle de usuários',
+    'alerted'       => 'Campo obrigatório*',    
+    'manager'       => 'Supervisor',
+    'tecCoo'        => 'Coordenador técnico',
+    'prodCoo'       => 'Coordenador de produção',
+    'engineer'      => 'Engenheiro',
+    'tech'          => 'Técnico',
+    'quick'         => 'Quick Repair',
+    's1'            => '[1T] Primeiro turno',
+    's2'            => '[2T] Segundo turno',
+    's3'            => '[3 T] Terceiro turno',
+    's4'            => 'ADM',
+    'save'          => 'SALVAR',
+    'login'         => 'Nome de usuário já registrado'
+);
