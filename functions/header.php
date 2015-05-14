a22949101ce481b99a324d0dacfb647be74fb9a0
Santaguida <santaguidafernando@gmail.com>
2015-05-14 11:21:11 -0300

    Changed link to URL

--- functions/header.php
+++ functions/header.php
@@ -10,12 +10,12 @@
     
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet">
         
-    <link rel="stylesheet" href="..\css/bootstrap.css">
-    <link rel="stylesheet" href="..\css/normalize.min.css">
-    <link rel="stylesheet" href="..\css/font-awesome.min.css">
-    <link rel="stylesheet" href="..\css/animate.css">
-    <link rel="stylesheet" href="..\css/templatemo-misc.css">
-    <link rel="stylesheet" href="..\css/templatemo-style.css">
-    <link rel="stylesheet" type="text/css" href="..\css/style.css">
+    <link rel="stylesheet" href="http://10.112.0.210/flexmo/css/bootstrap.css">
+    <link rel="stylesheet" href="http://10.112.0.210/flexmo/css/normalize.min.css">
+    <link rel="stylesheet" href="http://10.112.0.210/flexmo/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://10.112.0.210/flexmo/css/animate.css">
+    <link rel="stylesheet" href="http://10.112.0.210/flexmo/css/templatemo-misc.css">
+    <link rel="stylesheet" href="http://10.112.0.210/flexmo/css/templatemo-style.css">
+    <link rel="stylesheet" type="text/css" href="http://10.112.0.210/flexmo/css/style.css">
 
-    <script src="..\js/vendor/modernizr-2.6.2.min.js"></script> 
+    <script src="http://10.112.0.210/flexmo/js/vendor/modernizr-2.6.2.min.js"></script>
--- functions/menu1.php
+++ functions/menu1.php
@@ -8,7 +8,7 @@
                         <div class="logo">
                             <h1>
                             	<a href="http://10.112.0.210/flexmo">
-                            		<img src="..\images/banner.png" alt="Logo" ">                               
+                            		<img src="http://10.112.0.210/flexmo/images/banner.png" alt="Logo" ">                               
                         		</a>
                             </h1>
                         </div>
@@ -19,8 +19,8 @@
                                 <i class="fa fa-bars"></i>
                             </a>
                             <ul class="menu">
-                                <li><a href="..\user">Users</a></li>
-                        		<li><a href="..\team">Team</a></li>
+                                <li><a href="http://10.112.0.210/flexmo/user">Users</a></li>
+                        		<li><a href="http://10.112.0.210/flexmo/team">Team</a></li>
                             </ul>
                         </div>
                     </div>
@@ -34,9 +34,9 @@
                         <div class="list-menu">
                             <ul>
                                 <li><a href="http://10.112.0.210/flexmo">Home</a></li>
-                                <li><a href="..\user">User</a></li>                                
-                                <li><a href="..\team">Team</a></li>
-                                <li><a href="..\fct/FCT_Products.php">FCT</a></li>
+                                <li><a href="http://10.112.0.210/flexmo/user">User</a></li>                                
+                                <li><a href="http://10.112.0.210/flexmo/team">Team</a></li>
+                                <li><a href="http://10.112.0.210/flexmo/fct/forms/FCT_Products.php">FCT</a></li>
                             </ul>
                         </div>
                     </div>
@@ -48,12 +48,12 @@
 									$trans = "
 											Language | Idioma: 
 											
-											<a href='..\\functions/language.php?language=english&page=" . $page . "'>
-												<img src='..\images/eng.png' alt='English'
+											<a href='http://10.112.0.210/flexmo/functions/language.php?language=english&page=" . $page . "'>
+												<img src='http://10.112.0.210/flexmo/images/eng.png' alt='English'
 												style='width:40px;height:25px;border:0'>
 											</a>
-											<a href='..\\functions/language.php?language=portuguese&page=" . $page . "'>
-												<img src='..\images/ptbr.png' alt='Portuiguês' 
+											<a href='http://10.112.0.210/flexmo/functions/language.php?language=portuguese&page=" . $page . "'>
+												<img src='http://10.112.0.210/flexmo/images/ptbr.png' alt='Portuiguês' 
 												style='width:45px;height:25px;border:0'>
 											</a>
 									";
