f97d7647e695d2994952679abeb564469322b69f
Santaguida <santaguidafernando@gmail.com>
2015-05-19 10:08:33 -0300

    FCT Parts finished

--- fct/forms/FCT_Parts.php
+++ fct/forms/FCT_Parts.php
@@ -121,11 +121,4 @@ if(isset($_POST['product'])){
 	<!-- include menu2 end -->
     
 </body>
-</html><?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+</html>
