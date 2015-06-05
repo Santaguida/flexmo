7a1b65d9b96061fb4c0a2f0b5ea83c18ec25c50a
Santaguida <santaguidafernando@gmail.com>
2015-06-05 09:37:38 -0300

    FCT SETUP UPDATE

---
+++ fct/setup/update.php
@@ -0,0 +1,176 @@
+<!-- 
+    Developed by Fernando Henrique Santaguida and Gabriel Nazato
+    			http://www.fernandohs.com.br
+-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    
+<?php
+
+// Chama função que conecta o server
+require_once '..\..\functions/server.php';
+
+?>
+    
+<head>
+    
+    <!--
+    include head start
+    -->
+    
+    <?php include_once '..\..\functions/header.php'; ?>   
+        
+	<!--
+    include head end
+    -->
+    
+	<title>FleXmo</title>
+
+</head>
+
+<body>
+    
+<?php
+	
+	
+	$alert="";
+	
+	//In case there's no GET info
+	if(!isset($_GET['id']) && !isset($_POST['id']))
+	{
+		die("Error: GET or POST info not set! <br><br><a href='javascript:history.go(-1)' title='Go back to last page'>Back</a>");
+	}
+	
+	//In case there is POST info
+	if(isset($_POST['id']))
+	{	
+		//Check if the jig is due
+		$sql="SELECT id FROM tbl_fct_setup WHERE jigid='" . $_POST['jigid'] . "' AND product='" . $_POST['product'] . "'";
+		$result = check_data($sql) or die("Error: " . mysqli_error($db_link));
+		if(mysqli_num_rows($result)>=1)
+		{
+			$alert = "Possivel duplicidade";
+			$jig_id = $_POST['id'];
+		}
+		else
+		{		
+                    
+                        //Mount sql string
+			$sql = "UPDATE tbl_fct_setup SET product='" . $_POST['product'] . "', jigid='" . $_POST['jigid'] . "' WHERE id=" . $_POST['id'];
+			
+			//Execute query
+			check_data($sql) or die("Error: " . mysqli_error($db_link));
+			
+			//Go back to setup page
+			header('Location: index.php');
+                        
+                        echo 'teste';
+		}		
+	}
+	else
+	{
+		$jig_id = $_GET['id'];
+	}
+		
+	//Mount sql string
+	$sql = "SELECT
+                    s.id Id,
+                    s.jigid Jigid,
+                    s.station Station,
+                    p.product Product
+                FROM
+                    `tbl_fct_setup` s
+                JOIN
+                    `tbl_products` p ON (s.product=p.id)
+                WHERE s.id=" . $jig_id;
+			
+	//Execute query
+	$result = check_data($sql) or die("Erro: " . mysqli_error($db_link));
+	
+	$row = mysqli_fetch_array($result);
+	
+    $id = $row['Id'];
+    $product = $row['Product'];
+    $station = $row['Station'];
+    $jig_id = $row['Jigid'];
+    
+    
+?>
+
+<!-- include menu1 start -->
+
+<?php include_once '..\..\functions/menu1.php'; ?>
+
+    <!-- include menu1 end -->
+
+
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-5 col-sm-6">
+            <h3 align="center" class="widget-title">Setup</h3>
+<div class="contact-form">
+    <form name="setup_edit" action="" method="post">
+        <p>
+            <input name="id" type="hidden" value=" <?php echo $id; ?> "/>
+            <input type="text" name="station" id="station" value="<?php echo "Station: " . $station; ?>" disabled/>
+        </p>    
+        <p>
+            <table cellspacing="0" cellpadding="0" border="0">
+                <tr>
+                    <td width="100%">
+                        <select name="product" id="product" onChange="jsColorSwitch();">
+                            <?php
+                                //Query info for product <select>
+                                $query = "SELECT * FROM tbl_products WHERE enabled=1 ORDER BY account ASC";
+                                $result = check_data($query) or die("Erro: " . mysqli_error($db_link));
+                                                    
+                                for($i=1; $query_product = mysqli_fetch_array($result); $i++)
+                                {
+                                    //echo $query_product['id'];
+                                    $selected = "";
+                                    if($product==$query_product['product']){ $selected="selected"; }
+									
+                                    echo "<option value='" . $query_product['id'] . "' " . $selected . " style='color:#000000 !important;' >" . $query_product['product'] . "</option>";
+                                }					
+                            ?>
+                        </select>
+                    </td>                    
+                </tr>
+            </table>           
+        </p>
+        <p>
+            <input type="text" name="jigid" id="jigid" placeholder="<?php echo $jig_id; ?>" />
+        </p>
+        
+        <?php
+                //In case $invalid_login is set to "true", warns of wrong user
+                if($alert <> "")
+                {
+                    echo "<h5 class='red_danger'>" . $alert . "</h5>";
+                }
+                else
+                {
+                    echo "<br>";
+                }
+        ?>
+        
+        <p>
+            <input type="submit" class="button_blue" id="submit" value="Save">
+        </p>
+        
+    </form>
+</div>
+</div>
+</div>
+</div>
+    <!-- include menu2 start -->
+    
+<?php include_once '..\..\functions/menu2.php'; ?>   
+
+	<!-- include menu2 end -->
+    
+</body>
+</html>
---
+++ fct/setup/update_test.php
@@ -0,0 +1,161 @@
+<?php session_start(); ?>
+
+<!-- 
+    Developed by Fernando Henrique Santaguida and Gabriel Nazato
+    			http://www.fernandohs.com.br
+-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    
+<?php
+
+// Chama função que conecta o server
+require_once '..\..\functions/server.php';
+
+if(isset($_POST['id'])){
+    $id         = $_POST['id'];
+    $product    = $_POST['product'];
+    $jigid      = $_POST['jigid'];    
+    $alert      = '';
+
+    if($project == "0"){
+        $alert .= 'Informe o produto !';       
+    }
+    if(empty($jigid)){
+        $alert .= 'Informe a ID da jiga !';       
+    }
+    if(empty($alert)){
+        // Atualiza as informações no banco de dados
+        check_data("UPDATE
+                        tbl_fct_setup
+                    SET
+                        product = '$product',
+                        jigid = '$jigid'
+                    WHERE
+                        id = '$id'");
+        
+        header("location: index.php");
+    }
+}
+if(isset($_GET['id'])){
+    
+    $infoJig = check_data("SELECT
+                            s.id Id,
+                            s.jigid Jigid,
+                            s.station Station,
+                            p.product Product
+                        FROM
+                            `tbl_fct_setup` s
+                        JOIN
+                            `tbl_products` p ON (s.product=p.id)
+                        WHERE s.id =". $_GET['id']."");
+    
+    $jigData = mysqli_fetch_array($infoJig);
+    
+    $id         = (int)$jigData['Id'];
+    $product    = $jigData['Product'];
+    $product    = $jigData['Station'];
+    $jigid      = $jigData['Jigid'];
+    $alert      = '';
+}
+?>
+    
+<head>
+    
+    <!--
+    include head start
+    -->
+    
+    <?php include_once '..\..\functions/header.php'; ?>   
+        
+	<!--
+    include head end
+    -->
+    
+	<title>FleXmo</title>
+
+</head>
+
+<body>
+
+<!-- include menu1 start -->
+
+<?php include_once '..\..\functions/menu1.php'; ?>
+
+    <!-- include menu1 end -->       
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-sm-6">
+                    <h3 class="widget-title">Setup de jiga</h3>
+                     <?php
+                            
+                        // Caso exista um alerta, imprime na tela
+                        // Caso não, mantem mensagem HTML
+                        if(!empty($alert)){
+                            print '</br>' . $alert;
+                                }                            
+                        else{
+                            print 'Campo obrigatório*';
+                            }                           
+                            
+                    ?>
+                    
+                    <div class="contact-form">
+                      <form name="contactform" id="contactform" action="" method="post">
+                            <p>
+                                <select name="product" id="product" onChange="jsColorSwitch();">
+                                <option value="0" <?php if(!isset($product)){ echo "selected"; } ?> style="color:#a9a9a9;" >Product [Produto]</option>
+                                <?php
+                                    //Query info for account <select>
+                                    $query = "SELECT * FROM tbl_products WHERE enabled=1 ORDER BY product ASC";
+                                    $result = check_data($query) or die("Error: " . mysqli_error($db_link));
+                                    
+                                    for($i=1; $query_product = mysqli_fetch_array($result); $i++)
+                                    {
+                                        //echo $query_product['id'];
+                                        $selected = "";
+                                        if(isset($product))
+                                        {
+                                            if($product==$query_product['id']){ $selected="selected"; }
+                                        }
+                                        echo "<option value='" . $query_product['id'] . "' " . $selected . " style='color:#000000 !important;' >" . $query_product['product'] . "</option>";
+                                    }
+                                ?>
+                                </select>
+                            </p>
+                            <p>
+                                <select name="line">
+                                    <option value="0" <?php if(!isset($line)){ echo "selected"; } ?> style="color:#a9a9a9;" >Line [Linha]</option>
+                                    <?php for($i = 1; $i <= 16; $i++): ?>
+                                        <option value="<?php print $i . 'A'; ?>"><?php print $i . 'A'; ?></option>
+                                        <option value="<?php print $i . 'B'; ?>"><?php print $i . 'B'; ?></option>
+                                    <?php endfor ?>
+            </select>
+                            </p>
+                            <p>
+                                <select name="station">
+                                    <option value="0" <?php if(!isset($station)){ echo "selected"; } ?> style="color:#a9a9a9;" >Station [Estação]</option>
+                                    <?php for($i = 1; $i <= 50; $i++): ?>
+                                        <option value="<?php print $i; ?>"><?php print $i; ?></option>
+                                    <?php endfor ?>
+            </select>
+                            </p>
+                                <input name="jigid" type="text" id="jigid" placeholder="ID da jiga" value="<?php if(isset($jigid) && $alert != 'Salvo com sucesso !') { echo $jigid; } ?>" required>
+                            <p>
+                              <input type="submit" class="mainBtn" id="submit" value="Salvar">
+                            </p>                            
+                      </form>
+                    </div> <!-- /.contact-form -->
+                </div>
+                
+            </div>
+        </div>
+	<!-- include menu2 start -->
+    
+<?php include_once '..\..\functions/menu2.php'; ?>   
+
+	<!-- include menu2 end -->
+    
+</body>
+</html>
